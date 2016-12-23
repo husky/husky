fc27260ae18397bb55050c019bff40965c76edcd
Paul Bovbel <pbovbel@clearpath.ai>
2016-12-23 13:16:47 -0500

    Purge more UR; Implement urdf_extras

--- husky_description/env-hooks/50.husky_description.sh
+++
@@ -1,1 +0,0 @@
-export HUSKY_DESCRIPTION=$(catkin_find --without-underlays --first-only husky_description urdf/description.xacro 2>/dev/null)
--- husky_gazebo/env-hooks/50.husky_gazebo.sh
+++
@@ -1,1 +0,0 @@
-export HUSKY_GAZEBO_DESCRIPTION=$(catkin_find --without-underlays --first-only husky_gazebo urdf/description.gazebo.xacro 2>/dev/null)
