f3abe4407283d87753e767267873d4a965dcc71f
Dave Niewinski <dniewinski@clearpathrobotics.com>
2020-07-31 11:30:48 -0400

    Removed env-hooks

--- husky_bringup/env-hooks/50.husky_find_mag_config.sh
+++
@@ -1,6 +0,0 @@
-HUSKY_MAG_CONFIG=$(catkin_find --etc --first-only husky_bringup mag_config.yaml 2>/dev/null)
-if [ -z "$HUSKY_MAG_CONFIG" ]; then
-  HUSKY_MAG_CONFIG=$(catkin_find --share --first-only husky_bringup config/mag_config_default.yaml 2>/dev/null)
-fi
-
-export HUSKY_MAG_CONFIG
