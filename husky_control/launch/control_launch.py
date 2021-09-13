32840f1c8cf858f28c0e8f2605df89b1c7926e42
Tony Baltovski <tbaltovski@clearpathrobotics.com>
2021-09-13 10:17:43 -0400

    [husky_control] Added basic localization config.

--- husky_control/launch/control_launch.py
+++ husky_control/launch/control_launch.py
@@ -1,78 +1,20 @@
-
-
-import os
-
-from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
-from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
-from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
-
-import xacro
-
+from ament_index_python.packages import get_package_share_directory
+import launch_ros.actions
+import os
+import yaml
+from launch.substitutions import EnvironmentVariable
+import pathlib
+import launch.actions
+from launch.actions import DeclareLaunchArgument
 
 def generate_launch_description():
-
-    robot_description_path = os.path.join(
-         get_package_share_directory('husky_description'),
-        'urdf',
-        'husky.urdf.xacro',
-    )
-
-    robot_description_config = xacro.process_file(robot_description_path)
-    robot_description = {"robot_description": robot_description_config.toxml()}
-
-    husky_diff_drive_controller = os.path.join(
-        get_package_share_directory("husky_control"),
-        "config",
-        "control.yaml",
-    )
-
-    node_robot_state_publisher = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        output="screen",
-        parameters=[robot_description],
-    )
-
-    controller_manager_node = Node(
-        package="controller_manager",
-        executable="ros2_control_node",
-        parameters=[robot_description, husky_diff_drive_controller],
-        output={
-            "stdout": "screen",
-            "stderr": "screen",
-        },
-    )
-
-    # husky_base_node = Node(
-    #     package="husky_base",
-    #     executable="husky_node",
-    #     output="screen",
-    #     parameters=[robot_description],    
-    # )
-
-    spawn_controller = Node(
-        package="controller_manager",
-        executable="spawner.py",
-        arguments=["joint_state_broadcaster"],
-        output="screen",
-    )
-
-    spawn_dd_controller = Node(
-        package="controller_manager",
-        executable="spawner.py",
-        arguments=["husky_velocity_controller"],
-        output="screen",
-    )
-
-    return LaunchDescription(
-        [
-            node_robot_state_publisher,
-            controller_manager_node,
-            # husky_base_node,
-            spawn_controller,
-            spawn_dd_controller,
-        ]
-    )
+    return LaunchDescription([
+        launch_ros.actions.Node(
+            package='robot_localization',
+            executable='ekf_node',
+            name='ekf_node',
+            output='screen',
+            parameters=[os.path.join(get_package_share_directory("husky_control"), 'config', 'localization.yaml')],
+           ),
+])
