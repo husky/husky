16c60f6c393e8194d607401b65cc17da36725a1a
Tony Baltovski <tbaltovski@clearpathrobotics.com>
2022-04-16 16:27:48 -0400

    [husky_control] Cleaned up control_launch.py.

--- husky_control/launch/control_launch.py
+++ husky_control/launch/control_launch.py
@@ -1,20 +1,24 @@
 from launch import LaunchDescription
-from ament_index_python.packages import get_package_share_directory
-import launch_ros.actions
-import os
-import yaml
-from launch.substitutions import EnvironmentVariable
-import pathlib
-import launch.actions
-from launch.actions import DeclareLaunchArgument
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
 
 def generate_launch_description():
-    return LaunchDescription([
-        launch_ros.actions.Node(
-            package='robot_localization',
-            executable='ekf_node',
-            name='ekf_node',
-            output='screen',
-            parameters=[os.path.join(get_package_share_directory("husky_control"), 'config', 'localization.yaml')],
-           ),
-])
+    config_husky_ekf = PathJoinSubstitution(
+        [FindPackageShare("husky_control"),
+        "config",
+        "localization.yaml"],
+    )
+
+    node_ekf = Node(
+        package='robot_localization',
+        executable='ekf_node',
+        name='ekf_node',
+        output='screen',
+        parameters=[config_husky_ekf],
+        )
+    ld = LaunchDescription()
+    ld.add_action(node_ekf)
+
+    return ld
