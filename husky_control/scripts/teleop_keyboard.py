173de760ed9817b9833fb72409c0f231fbaa6cba
Tinker Twins <tinkertwins22@gmail.com>
2022-08-08 11:02:54 -0400

    Update teleop_keyboard.py

--- husky_control/scripts/teleop_keyboard.py
+++ husky_control/scripts/teleop_keyboard.py
@@ -14,3 +14,22 @@ LIN_VEL_LIMIT = 1.0 # m/s
 ANG_VEL_LIMIT = 2.0 # rads
 LIN_VEL_STEP  = 0.1
 ANG_VEL_STEP  = 0.1
+
+info = """
+----------------------------------------
+Husky Robot Keyboard Teleoperation Panel
+----------------------------------------
+                 W
+             A   S   D
+                 X
+W/S : Increase/decrease linear velocity
+D/A : Increase/decrease angular velocity
+X   : Emergency brake
+Press CTRL+C to quit
+NOTE: Press keys within this terminal
+----------------------------------------
+"""
+
+error = """
+ERROR: Communication failed!
+"""
