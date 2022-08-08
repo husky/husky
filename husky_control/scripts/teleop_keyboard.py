060540d101128fa785931153df76c3418cea9841
Tinker Twins <tinkertwins22@gmail.com>
2022-08-08 11:06:57 -0400

    Update teleop_keyboard.py

--- husky_control/scripts/teleop_keyboard.py
+++ husky_control/scripts/teleop_keyboard.py
@@ -81,3 +81,6 @@ if __name__=="__main__":
 
     rospy.init_node('husky_teleop_keyboard')
     cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+
+    lin_vel = 0.0
+    ang_vel = 0.0
