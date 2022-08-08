07ba2c836666b10727c2f90f5c0e94bdcf3cd075
Tinker Twins <tinkertwins22@gmail.com>
2022-08-08 11:02:34 -0400

    Update teleop_keyboard.py

--- husky_control/scripts/teleop_keyboard.py
+++ husky_control/scripts/teleop_keyboard.py
@@ -9,3 +9,8 @@ else:
   import tty, termios
 
 ################################################################################
+
+LIN_VEL_LIMIT = 1.0 # m/s
+ANG_VEL_LIMIT = 2.0 # rads
+LIN_VEL_STEP  = 0.1
+ANG_VEL_STEP  = 0.1
