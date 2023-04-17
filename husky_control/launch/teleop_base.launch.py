f054d46b7724794f040c86df118b23c576dcb0fd
Nicolas Dubois <nicolas.dubois.ext@exxact-robotics.com>
2023-04-17 16:47:40 +0200

    re add interactive marker twist server

--- husky_control/launch/teleop_base.launch.py
+++ husky_control/launch/teleop_base.launch.py
@@ -14,14 +14,14 @@ def generate_launch_description():
     )
 
 
-    # node_interactive_marker_twist_server = Node(
-    #     package='interactive_marker_twist_server',
-    #     executable='marker_server',
-    #     name='twist_server_node',
-    #     remappings={('cmd_vel', 'twist_marker_server/cmd_vel')},
-    #     parameters=[filepath_config_interactive_markers],
-    #     output='screen',
-    # )
+    node_interactive_marker_twist_server = Node(
+        package='interactive_marker_twist_server',
+        executable='marker_server',
+        name='twist_server_node',
+        remappings={('cmd_vel', 'twist_marker_server/cmd_vel')},
+        parameters=[filepath_config_interactive_markers],
+        output='screen',
+    )
 
     node_twist_mux = Node(
         package='twist_mux',
@@ -32,7 +32,7 @@ def generate_launch_description():
     )
 
     ld = LaunchDescription()
-    # ld.add_action(node_interactive_marker_twist_server)
+    ld.add_action(node_interactive_marker_twist_server)
     ld.add_action(node_twist_mux)
     return ld
 
